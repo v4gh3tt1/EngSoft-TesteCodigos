da98086e07da0fe701d5b858a1e8b5ed53b70f92
Charles Vaghetti <63205923+v4gh3tt1@users.noreply.github.com>
2020-11-11 16:13:43 -0300

    Funcao calcula dois numeros rodando

--- src/TesteCodigos.java
+++ src/TesteCodigos.java
@@ -1,7 +1,14 @@
-public class TesteCodigos {
+public class TesteCodigos { //soma dois numeros
 
+    static int x, y, resultado;
+    static int soma (int a, int b) {
+        return (a+b);
+    }
     public static void main(String[] args) {
-
-        System.out.println("Hello World");   
+    x = 5;
+    y = 3;
+    resultado = soma(x, y);
+    System.out.println(resultado);
+      
     }
 }
