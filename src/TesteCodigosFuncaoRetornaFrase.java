8a205122ff91f7dceee930390bc18ebfc9aeb412
Charles Vaghetti <63205923+v4gh3tt1@users.noreply.github.com>
2020-11-11 16:55:24 -0300

    Procedimento imprime mensagem na tela

---
+++ src/TesteCodigosFuncaoRetornaFrase.java
@@ -0,0 +1,12 @@
+public class TesteCodigosFuncaoRetornaFrase { //invoca funcao que imprime mensagem na tela
+
+    static void frase () {
+        System.out.println("Hello World!");
+    }
+    
+    public static void main(String[] args) {  
+
+    frase();
+  
+    }
+}
