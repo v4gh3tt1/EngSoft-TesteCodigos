9f1de503a1043381780cf6ac78a7722092285d31
Charles Vaghetti <63205923+v4gh3tt1@users.noreply.github.com>
2020-11-12 08:56:51 -0300

    Merge modificações

--- src/TesteCodigosProcedimentoRetornaFrase.java
+++ src/TesteCodigosProcedimentoRetornaFrase.java
@@ -1,27 +1,19 @@
-<<<<<<< HEAD
-public class TesteCodigosProcedimentoRetornaFrase { //invoca Procedimento que imprime mensagem na tela
 
-    static void frase () {
-=======
 public class TesteCodigosProcedimentoRetornaFrase { //invoca Procedimento que imprime mensagem na tela, com e sem parametro
 
     static void frase (String msg) { //procedimento sem parametro
         System.out.println(msg);
     }
     static void frase () { //procedimento com parametro
->>>>>>> Codes
         System.out.println("Hello World!");
     }
     
     public static void main(String[] args) {  
 
-<<<<<<< HEAD
-    frase();
-=======
         String mensagem = ("Hello World!");
         frase(mensagem); // invoca procediemento e passa o parametro
         frase(); // invoca procedimento sem parametro
->>>>>>> Codes
+
   
     }
 }
