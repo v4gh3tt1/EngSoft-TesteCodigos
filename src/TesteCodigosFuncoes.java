e0bd1bbf690265a2f60102be71833f20f239dc12
Charles Vaghetti <63205923+v4gh3tt1@users.noreply.github.com>
2020-11-12 09:30:02 -0300

    Funcçoes com e sem parametros

---
+++ src/TesteCodigosFuncoes.java
@@ -0,0 +1,20 @@
+
+public class TesteCodigosFuncoes {
+
+    static String frase(){ // funcao sem parametro
+        String mensagem = "Hello World!";
+        return mensagem;
+    }
+
+    static String frase2(String msg){ // funcao esperando parametro
+        String mensagem = msg;
+        return mensagem;
+    }
+    public static void main(String[] args) {
+        
+        System.out.println(frase());
+        
+        String texto = frase2("Hello Hello!"); // invocação da funcao repassando parametro
+        System.out.println(texto); 
+    }
+}
