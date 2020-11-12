2286d5e77fd05340525264e7f4ee5a96ed4c022f
Charles Vaghetti <63205923+v4gh3tt1@users.noreply.github.com>
2020-11-12 08:47:59 -0300

    Procedimento usando strings, com e sem parametros

---
+++ src/TesteCodigosProcedimentoRetornaFrase.java
@@ -0,0 +1,17 @@
+public class TesteCodigosProcedimentoRetornaFrase { //invoca Procedimento que imprime mensagem na tela, com e sem parametro
+
+    static void frase (String msg) { //procedimento sem parametro
+        System.out.println(msg);
+    }
+    static void frase () { //procedimento com parametro
+        System.out.println("Hello World!");
+    }
+    
+    public static void main(String[] args) {  
+
+        String mensagem = ("Hello World!");
+        frase(mensagem); // invoca procediemento e passa o parametro
+        frase(); // invoca procedimento sem parametro
+  
+    }
+}
